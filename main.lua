ba4999fa1e92a52b04c57e22f481cd1a0d660f8a
Eric Beyer <e.beyer@techsmith.com>
2013-03-18 21:17:49 -0400

    Adding some comments to the file to give a better idea of what I'm trying to do in each block of code.

--- main.lua
+++ main.lua
@@ -1,3 +1,4 @@
+-- Setup screen dimensions
 screenWidth = MOAIEnvironment.screenWidth
 screenHeight = MOAIEnvironment.screenHeight
 if screenWidth == nil then screenWidth = 480 end
@@ -5,6 +6,7 @@ if screenHeight == nil then screenHeight = 800 end
 
 MOAISim.openWindow("Window",screenWidth,screenHeight)
 
+-- Include required libraries
 package.path = './moaigui/?.lua;' .. './LuaXml/?.lua;' .. package.path
 require("LuaXml")
 require "lfs"
@@ -22,6 +24,7 @@ gui:addToResourcePath(filesystem.pathJoin("moaigui/resources", "gui"))
 gui:addToResourcePath(filesystem.pathJoin("moaigui/resources", "media"))
 gui:addToResourcePath(filesystem.pathJoin("moaigui/resources", "themes"))
 
+-- Setup view port and objects to be displayed on screen
 viewport = MOAIViewport.new()
 viewport:setSize(screenWidth,screenHeight)
 viewport:setScale(screenWidth,screenHeight)
@@ -46,6 +49,7 @@ layermgr.addLayer("gui",1, gui:layer())
 gui:setTheme("basetheme.lua")
 gui:setCurrTextStyle("default")
 
+-- Setup functions to pass clicks and touches to the MOAIGUI
 function onPointerEvent(x, y)
     gui:injectMouseMove(x, y)
 end
@@ -68,7 +72,7 @@ function onTouchEvent(eventType,idx,x,y,tapCount)
     end
 end
 
-
+-- Register the device's selection tool
 if MOAIInputMgr.device.pointer then
     MOAIInputMgr.device.pointer:setCallback(onPointerEvent)
     MOAIInputMgr.device.mouseLeft:setCallback(onMouseLeftEvent)
@@ -76,45 +80,43 @@ else
     MOAIInputMgr.device.touch:setCallback(onTouchEvent)
 end
 
-function nextGame()
-	return math.random(300) % 5 + 1
+-- When button is clicked load the next game module and increase the counter
+function onButtonClick(event,data)
+	local fileName = gameList[gameIndex]
+	if fileName ~= nil then
+		setupNextGame(fileName)
+		gameIndex = gameIndex + 1
+		-- If exhausted all games in the list, reset counter to start over
+		if (gameIndex == #gameList+1) then
+			gameIndex = 1
+		end
+	end
+	displayGameData()
 end
 
 function setupNextGame(gamename)
-	-- some pseudo random way of establishing the next game
 	local path = "./gamedata/" .. gamename .. "/data.xml"
 	local xfile = xml.load(path)
-
 	local currentGame = xfile:find("game")
 
+	-- Using messy xml parsing.. Some day there should be a better way to parse the xml
 	if currentGame ~= nil then
 		currentTitle = currentGame:find("title")[1]
 		currentTimespanMin = currentGame:find("time"):find("min")[1]
 		currentTimespanMax = currentGame:find("time"):find("max")[1]
 		currentImage = currentGame:find("image")[1]
-	end
-
-	currentLabel = "Currently playing: " .. currentTitle .. ", you have " .. currentTimespanMin .. "-" .. currentTimespanMax .. " seconds."
-	print(currentLabel)
+		currentLabel = "Currently playing: " .. currentTitle .. ", you have " .. currentTimespanMin .. "-" .. currentTimespanMax .. " seconds."
+	end	
 end
 
+-- Currently all we have is a label and image displayed to the screen. This method should update any elements the xml contains
 function displayGameData()
 	label1:setText(currentLabel)
 	texture:load(currentImage)
 	sprite:setTexture(texture)
 end
 
-function onButtonClick(event,data)
-	local fileName = gameList[gameIndex]
-	if fileName ~= nil then
-		setupNextGame(fileName)
-		gameIndex = gameIndex + 1
-	else
-		setupNextGame("Default")
-	end
-	displayGameData()
-end
-
+-- Set up the gui elements
 button = gui:createButton()
 button:setPos(0, 85)
 button:setDim(100,15)
@@ -128,12 +130,15 @@ label1:setDim(100,15)
 label1:setText("Default Label")
 label1:setTextAlignment(label1.TEXT_ALIGN_CENTER)
 
+
+-- This is where the initial start screen would be loaded from
 setupNextGame("Default")
 displayGameData()
 gameIndex = 1
 
 gameList = {}
 
+-- looks at the gamedata directory and populates the gameList table with the names of each directory (which are the names of the games).
 local iter = 1
 for file in lfs.dir[[./gamedata]] do
 	if file ~= "Default" and file ~= "." and file ~= ".."then
